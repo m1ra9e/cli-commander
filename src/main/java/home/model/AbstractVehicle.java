f6bdf4ae6ecd29a67d985aed2ffb1ef782c232ea
l.shamsutdinov <l.shamsutdinov@mail.ru>
2025-06-15 18:22:59 +0300

    added raw versions of models

---
+++ src/main/java/home/model/AbstractVehicle.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2025 Lenar Shamsutdinov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+package home.model;
+
+import java.util.Objects;
+
+public abstract sealed class AbstractVehicle permits Car, Truck, Motorcycle {
+
+    private VehicleType type;
+    private String color;
+    private String number;
+
+    public AbstractVehicle(VehicleType type) {
+        this.type = type;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public void setType(VehicleType type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, color, number);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof AbstractVehicle other)) {
+            return false;
+        }
+        return type == other.type
+                && Objects.equals(color, other.color)
+                && Objects.equals(number, other.number);
+    }
+}
